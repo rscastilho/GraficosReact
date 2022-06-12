7421a5fafc876bda74155a35cc11ba5468136f79
Rodrigo Castilho <rcastilho@gmail.com>
2022-06-12 16:45:44 -0300

    refresh title

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,7 +1,7 @@
 export const users = [
     { id: 1, name: 'Rodrigo', age: 40, amount: 1500 },
-    { id: 2, name: 'Emily', age: 10, amount: 1250 },
-    { id: 3, name: 'Elisete', age: 45, amount: 1000 },
+    { id: 2, name: 'Katarina', age: 10, amount: 1250 },
+    { id: 3, name: 'Eleonora', age: 45, amount: 1000 },
     { id: 4, name: 'Maria', age:34, amount: 1800 },
     { id: 5, name: 'João', age: 22, amount: 1505 },
     { id: 6, name: 'Gabriela', age: 66, amount: 1150 },
